3df683c80009fad0d1d66913dd0b34f9c3094e58
Ta-2 <nakao.tatuya.2000@gmail.com>
2021-02-23 14:00:59 +0900

    update thread.cpp

--- thread.cpp
+++ thread.cpp
@@ -1,15 +1,18 @@
 #include <utility>
 #include <iostream>
+#include <string.h>
+#include <conio.h>
 #include <boost/thread.hpp>
 #define arr_size 10
+boost::mutex stdmtx;
 
 class MemoryManager {
   double *front_arr = nullptr, *back_arr = nullptr;
   boost::shared_mutex mtx_m;
 public:
   MemoryManager (int size){
-    front_arr = new double[size];
-    back_arr = new double[size];
+    front_arr = new double[size]();
+    back_arr = new double[size]();
   }
   double *GetFrontArr (){
     boost::shared_lock<boost::shared_mutex> lock(mtx_m);
@@ -23,6 +26,8 @@ public:
     boost::upgrade_lock<boost::shared_mutex> lock(mtx_m);
     {
       boost::upgrade_to_unique_lock<boost::shared_mutex> write_lock(lock);
+      boost::lock_guard<boost::mutex> guard(stdmtx);
+      std::cout << "arr swaped" << std::endl;
       std::swap(front_arr, back_arr);
       return back_arr;
     }
@@ -41,9 +46,9 @@ public:
     target_arr = mm_m->GetBackArr();
   }
   void Analyze (){
-    for(int i = 0; i < size_m; i++) *(target_arr+i)=+1;
+    for(int i = 0; i < size_m; i++)target_arr[i] += 1;
   }
-    void SetArr (double *arr){ target_arr = arr; }
+    void SetArr (){ target_arr = mm_m->GetBackArr(); }
     void UpdateArr (){ target_arr = mm_m->SwapArr(); }
 };
 
@@ -58,33 +63,64 @@ public:
     mm_m = mm;
     target_arr = mm_m->GetBackArr();
   }
-  void Send () const {
+  void Send () {
+    boost::lock_guard<boost::mutex> guard(stdmtx);
     std::cout << step << ": ";
     for(int i = 0; i < size_m; i++) std::cout << *(target_arr+i);
     std::cout << std::endl << std::endl;
+    step++;
   }
-  void SetArr (double *arr){ target_arr = arr; }
+  void SetArr (){ target_arr = mm_m->GetFrontArr(); }
 };
 
-void AnalyzerThread (MemoryManager *mm){
+void AnalyzerThread (MemoryManager *mm, bool *flag){
   Analyzer a(arr_size, mm);
-
+  a.SetArr();
+  while(*flag){
+    a.Analyze();
+    a.UpdateArr();
+    boost::this_thread::sleep(boost::posix_time::milliseconds(100));
+  }
   return;
 }
 
-void SenderThread (MemoryManager *mm){
+void SenderThread (MemoryManager *mm, bool *flag){
   Sender s(arr_size, mm);
-
+  s.SetArr();
+  while(*flag){
+    //fprintf(stdout, "t2\n");
+    s.Send();
+    s.SetArr();
+    boost::this_thread::sleep(boost::posix_time::milliseconds(100));
+  }
   return ;
 }
 
-int main(){
-  MemoryManager *mm = new MemoryManager(arr_size);
-  boost::thread AT( boost::bind(AnalyzerThread, mm));
-  boost::thread ST( boost::bind(SenderThread, mm));
+void InputThread (bool *flag){
+  std::string input;
+  while(true){
+    std::cin >> input;
+    if(input == "q"){
+      *flag = false;
+      break;
+    }
+  }
+  return;
+}
 
+int main(){
+  bool flag = true;
+  MemoryManager *mm = new MemoryManager(arr_size);
+  boost::thread AT( boost::bind(AnalyzerThread, mm, &flag));
+  boost::thread ST( boost::bind(SenderThread, mm, &flag));
+  boost::thread IT( boost::bind(InputThread, &flag));
+
+  IT.join();
+  fprintf(stdout, "IT finished\n");
   AT.join();
+  fprintf(stdout, "AT finished\n");
   ST.join();
+  fprintf(stdout, "ST finished\n");
 
   return 0;
 }
